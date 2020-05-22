9bb3a7ff8be536ec9a63f14d04a7f27fca1d6f21
Nassim55 <52613668+Nassim55@users.noreply.github.com>
2020-05-22 20:27:22 +0100

    22/05/2020 Commit 1

--- src/App.js
+++ src/App.js
@@ -2,30 +2,59 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [sentences, setSentences] = useState([]);
+  const [sentence, setSentence] = useState('');
+  const [isSentenceSarcastic, setIsSentenceSarcastic] = useState('');
 
   const fetchData = async () => {
+    console.log('fetchData() has been called')
     const response = await fetch('/ml');
     const data = await response.json();
-    setSentences(data.sentences);
+    setIsSentenceSarcastic(data['isSarcastic']);
   };
+
+  const fetchDataPost = async () => {
+    console.log('fetchDataPost() has been called')
+    console.log(JSON.stringify(sentence));
+    const response = await fetch('/ml', {
+      method: 'POST',
+      body: JSON.stringify(sentence),
+      headers:{"content_type": "application/json"}
+    });
+    const data = await response.json();
+    setIsSentenceSarcastic(data['isSarcastic']);
+    return data;
+  };
+  
 
   useEffect(() => {
     fetchData()
   }, []);
 
+  const handleSubmit = e => {
+    fetchDataPost()
+    e.preventDefault();
+
+  }
+
   return (
     <div className="App">
       <div className="search-form">
-        <form>
-          <input type="text"></input>
+        <form
+        method="post"
+        onSubmit={handleSubmit}>
+          <input
+          action=""
+          type="text"
+          name="nm"
+          onChange={e => setSentence(e.target.value)}
+          value={sentence}>
+          </input>
           <input type="submit"></input>
         </form>
       </div>
       <div className="search-results">
-        {sentences.map(sentence => (
-          <p>{sentence}</p>
-        ))}
+        <div className="input-sentence">{sentence}</div>
+        <div className="sarcasm-result">{isSentenceSarcastic}</div>
       </div>
     </div>
   );
