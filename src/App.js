048484bfd8a6af1817a5d6c87d90697a43b390f8
Nassim55 <52613668+Nassim55@users.noreply.github.com>
2020-05-23 05:03:38 +0100

    23/05/2020 Commit 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
+
 const App = () => {
   const [sentence, setSentence] = useState('');
   const [isSentenceSarcastic, setIsSentenceSarcastic] = useState('');
 
   const fetchData = async () => {
-    console.log('fetchData() has been called')
     const response = await fetch('/ml');
     const data = await response.json();
     setIsSentenceSarcastic(data['isSarcastic']);
   };
 
   const fetchDataPost = async () => {
-    console.log('fetchDataPost() has been called')
-    console.log(JSON.stringify(sentence));
     const response = await fetch('/ml', {
       method: 'POST',
       body: JSON.stringify(sentence),
@@ -25,7 +23,6 @@ const App = () => {
     return data;
   };
   
-
   useEffect(() => {
     fetchData()
   }, []);
@@ -38,24 +35,37 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="search-form">
-        <form
-        method="post"
-        onSubmit={handleSubmit}>
-          <input
-          action=""
-          type="text"
-          name="nm"
-          onChange={e => setSentence(e.target.value)}
-          value={sentence}>
-          </input>
-          <input type="submit"></input>
-        </form>
-      </div>
-      <div className="search-results">
-        <div className="input-sentence">{sentence}</div>
-        <div className="sarcasm-result">{isSentenceSarcastic}</div>
-      </div>
+      <form
+      className="search-form"
+      method="post"
+      onSubmit={handleSubmit}>
+        <div className="intro-title">
+          Is your sentence sarcastic? Lets find out
+        </div>
+        <input
+        className="search-input"
+        action=""
+        type="text"
+        placeholder="Search"
+        name="nm"
+        onChange={e => setSentence(e.target.value)}
+        value={sentence}>
+        </input>
+        <button type="submit" className="search-button">
+          <svg className="submit-button">
+          <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#search"></use>
+          </svg>
+        </button>
+        <div className="result">
+          {isSentenceSarcastic}
+        </div>
+      </form>
+      <svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" display="none">
+        <symbol id="search" viewBox="0 0 32 32">
+          <path d="M 19.5 3 C 14.26514 3 10 7.2651394 10 12.5 C 10 14.749977 10.810825 16.807458 12.125 18.4375 L 3.28125 27.28125 L 4.71875 28.71875 L 13.5625 19.875 C 15.192542 21.189175 17.250023 22 19.5 22 C 24.73486 22 29 17.73486 29 12.5 C 29 7.2651394 24.73486 3 19.5 3 z M 19.5 5 C 23.65398 5 27 8.3460198 27 12.5 C 27 16.65398 23.65398 20 19.5 20 C 15.34602 20 12 16.65398 12 12.5 C 12 8.3460198 15.34602 5 19.5 5 z" />
+        </symbol>
+      </svg>
+
     </div>
   );
 }
